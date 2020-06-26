6551c02a5aaabfeb35fa7248108be61c14e29b69
luisabianca <luisabianca93@gmail.com>
2020-06-25 22:34:48 -0300

    aula 3

--- scripts/personagem.js
+++ scripts/personagem.js
@@ -1,50 +1,72 @@
   
 class Personagem extends Animacao {
-  constructor(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite) {
-    super(matriz, imagem, x, largura, altura, larguraSprite, alturaSprite)
+  constructor(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite, velocidade) {
+    super(matriz, imagem, x, variacaoY, largura, altura, larguraSprite, alturaSprite)
 
-    this.yInicial = height - this.altura;
+    this.variacaoY = variacaoY;
+
+    this.yInicial = height - this.variacaoY;
     this.y = this.yInicial;
 
     this.velocidadePulo = 0;
     this.gravidade = 3;
 
-    this.qntPulos = 1;
+    this.qntPulos = 2;
   }
 
  pula() {
-    if (this.qntPulos > 0) {
-      this.velocidadePulo = -35;
-      this.qntPulos--;
-    }
+  if (this.qntPulos > 0) {
+   this.velocidadePulo = -35;
+   this.qntPulos--;
+   somPulo.play();
   }
+ }
 
-  aplicaGravidade() {
-    this.y += this.velocidadePulo;
-    this.velocidadePulo = this.velocidadePulo + this.gravidade;
+ aplicaGravidade() {
+  this.y += this.velocidadePulo;
+  this.velocidadePulo = this.velocidadePulo + this.gravidade;
 
-    if (this.y > this.yInicial) {
-      this.y = this.yInicial;
-      this.qntPulos = 1;
-    }
+  if(this.y > this.yInicial) {
+    this.y = this.yInicial;
+    this.qntPulos = 2;
   }
+ }
 
-  estaColidindo(inimigo) {
-    noFill();
-    const precisao = .65
-    const colisao = collideRectRect(
-      this.x, 
-      this.y, 
-      this.largura*precisao, 
-      this.altura,      
-      inimigo.x, 
-      inimigo.y,
-      inimigo.largura*precisao, 
-      inimigo.altura*precisao,
+ estaColidindo(inimigo) {
+  const precisao = .6;
+  const colisao = collideRectRect(
+    this.x, 
+    this.y, 
+    this.largura*precisao, 
+    this.altura*precisao,      
+    inimigo.x, 
+    inimigo.y,
+    inimigo.largura*precisao, 
+    inimigo.altura*precisao,
     
-    );
+  );
 
-    return colisao;
+  return colisao;
   
-  }
+ }
+ 
+
+ marcaPonto(pontos) {
+  const precisao = .6;
+  const marcaPonto = collideRectRect(
+    this.x, 
+    this.y, 
+    this.largura*precisao, 
+    this.altura*precisao,      
+    pontos.x, 
+    pontos.y,
+    pontos.largura*precisao, 
+    pontos.altura*precisao,
+    
+  );
+
+  return marcaPonto;
+  
+ }
+
 }
